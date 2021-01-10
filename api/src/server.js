b60edc01b073bdc53ee9e4e2a233b3c26e1193db
Polyhistor <pouya.ataei.7@gmail.com>
2021-01-10 16:42:22 +1300

    practicing resolvers, top level resolvers

--- api/src/server.js
+++ api/src/server.js
@@ -1,17 +1,29 @@
-const { ApolloServer } = require('apollo-server')
-const typeDefs = require('./schema')
-const resolvers = require('./resolvers')
-const {models, db} = require('./db')
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const { models, db } = require('./db/index');
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context() {
+//     const user = db.get('user').value()
+//     return {models, db, user}
+//   }
+// })
+
+// server.listen().then(({ url }) => {
+//   console.log(`🚀 Server ready at ${url}`);
+// })
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context() {
-    const user = db.get('user').value()
-    return {models, db, user}
-  }
-})
+    return { models, db };
+  },
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
-})
+});
